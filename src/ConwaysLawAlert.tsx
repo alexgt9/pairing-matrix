0818e5fd6b2482e44a668e0339929239768b1da2
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2021-10-28 11:58:49 +0200

    Move Conways alert

---
+++ src/ConwaysLawAlert.tsx
@@ -0,0 +1,20 @@
+const ConwaysLawAlert = () => {
+  return (
+    <div>
+      <p className="alert-box bg-orange-200 border-l-4 border-orange-500 text-orange-700 p-4">
+        The participants & rotation frequency numbers are so high that probably
+        you don't want to know the results,
+        <a
+          className="underline ml-2"
+          href="https://en.wikipedia.org/wiki/Conway%27s_law"
+          target="_blank"
+          rel="noreferrer"
+        >
+          check this out
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default ConwaysLawAlert;
--- src/PairingCalendar.tsx
+++ src/PairingCalendar.tsx
@@ -1,9 +1,8 @@
-import _ from "lodash";
 import CalendarFile from "./CalendarFile";
 import CalendarFooter from "./CalendarFooter";
 import CalendarTable from "./CalendarTable";
+import ConwaysLawAlert from "./ConwaysLawAlert";
 import { PairingDay, Rotations } from "./PairingApp";
-import { Pair } from "./types";
 import { getNextWorkingDay, leastCommonMultiple, nextMonday } from "./utils";
 
 const DAYS_A_WEEK = 5;
@@ -22,29 +21,30 @@ const PairingCalendar = ({
   description,
   untilDate,
 }: PairingCalendarProps) => {
+  // lcm(5 * 2 , 5) / 5 * 2
   const differentPairs = rotationDays.length;
+  const daysPerSingleCycle = differentPairs * rotationFrequency;
+  const timesToCompleteFullCycle =
+    leastCommonMultiple(daysPerSingleCycle, DAYS_A_WEEK) / daysPerSingleCycle;
 
-  const daysWithRepetition: Rotations[] = rotationDays.reduce(
+  const daysWithRotationFrequency: Rotations[] = rotationDays.reduce(
     (repeatedDays, day) => [
       ...repeatedDays,
       ...Array(rotationFrequency || DEFAULT_ROTATION_FREQUENCY).fill(day),
     ],
     [] as Rotations[]
   );
-
-  const neededRepetitions =
-    leastCommonMultiple(differentPairs * rotationFrequency, DAYS_A_WEEK) /
-    (differentPairs * rotationFrequency);
-  const daysUntilDate: Rotations[] = _.range(neededRepetitions)
-    .map(() => daysWithRepetition)
+  const daysUntilCompleteFullCycle: Rotations[] = Array(
+    timesToCompleteFullCycle
+  )
+    .fill(daysWithRotationFrequency)
     .flat();
 
   const repeatEveryWeeks =
-    leastCommonMultiple(differentPairs * rotationFrequency, DAYS_A_WEEK) /
-    DAYS_A_WEEK;
+    leastCommonMultiple(daysPerSingleCycle, DAYS_A_WEEK) / DAYS_A_WEEK;
 
   let nextDay = nextMonday();
-  const daysWithDate = daysUntilDate.map((day) => {
+  const daysWithDate = daysUntilCompleteFullCycle.map((day) => {
     const dayWithDate = {
       ...day,
       date: nextDay,
@@ -53,26 +53,13 @@ const PairingCalendar = ({
 
     return dayWithDate;
   });
-  const daysFilteredByEndDate = daysWithDate.filter((day: PairingDay) =>
-    untilDate ? day.date < untilDate : true
-  );
+  const daysFilteredByEndDate = untilDate ? 
+    daysWithDate.filter((day: PairingDay) => day.date < untilDate) :
+    daysWithDate
+  ;
 
-  return !neededRepetitions ? (
-    <div>
-      <p className="alert-box bg-orange-200 border-l-4 border-orange-500 text-orange-700 p-4">
-        The participants & rotation frequency numbers are so high that probably
-        you don't want to know the results,
-        <a
-          className="underline ml-2"
-          href="https://en.wikipedia.org/wiki/Conway%27s_law"
-          target="_blank"
-          rel="noreferrer"
-        >
-          check this out
-        </a>
-      </p>
-    </div>
-  ) : (
+  return !timesToCompleteFullCycle ? 
+    <ConwaysLawAlert/> : 
     <>
       <CalendarFile
         days={daysFilteredByEndDate}
@@ -90,58 +77,7 @@ const PairingCalendar = ({
         />
       </div>
     </>
-  );
-};
-
-type TableRowProps = {
-  rowNumber: number;
-  days: PairingDay[];
-};
-
-export const TableRow = ({ days, rowNumber }: TableRowProps) => {
-  const cells = days.map((day: PairingDay, index: number) => (
-    <TableCell
-      key={index}
-      pairs={day.pairs}
-      colorClass={day.colorClass}
-      coordinates={`${rowNumber}-${index}`}
-      date={day.date}
-    />
-  ));
-
-  return <tr key={rowNumber}>{cells}</tr>;
-};
-
-type TableCellProps = {
-  date: Date;
-  coordinates: string;
-  pairs: Pair[];
-  colorClass: string;
-};
-
-export const TableCell = ({
-  pairs,
-  colorClass,
-  coordinates,
-  date,
-}: TableCellProps) => {
-  const listItems = pairs.map((pair: Pair, index: number) => (
-    <li key={`${pair[0]}${pair[1]}`}>{`${pair[0]} & ${pair[1]}`}</li>
-  ));
-  const monthStyle =
-    coordinates === "0-0" || date.getDate() === 1 ? "long" : "short";
-
-  return (
-    <td
-      className="px-6 py-4 whitespace-nowrap border-gray-400"
-      key={coordinates}
-    >
-      <span className={"cell-date"}>
-        {date.toLocaleString("es-US", { month: monthStyle, day: "numeric" })}
-      </span>
-      <div className={`${colorClass} pair rounded-md`}>{listItems}</div>
-    </td>
-  );
+  ;
 };
 
 export default PairingCalendar;
