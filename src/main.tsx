5c9fb8741b52607cc24b5955f9a7c6c86b1b891b
Alejandro Batanero <abatanero@civitatis.com>
2023-02-17 09:23:50 +0100

    Use Hash router

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,11 @@ import Pairs from './pages/Pairs'
 import Calendar from './pages/Calendar'
 
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App/>,
